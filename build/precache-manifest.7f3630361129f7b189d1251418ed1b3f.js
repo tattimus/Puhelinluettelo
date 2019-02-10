5d2e17a98302159c5522af1e306bb9fbd1dc2942
Tattimus <samu.tatti@luukku.com>
2019-02-10 03:22:51 +0200

    frontend herokuun

---
+++ build/precache-manifest.7f3630361129f7b189d1251418ed1b3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "62ad43644b8d2bc7fe17",
+    "url": "/static/css/main.b4fe7f6f.chunk.css"
+  },
+  {
+    "revision": "62ad43644b8d2bc7fe17",
+    "url": "/static/js/main.62ad4364.chunk.js"
+  },
+  {
+    "revision": "1bdeb50c48bfe976fdd4",
+    "url": "/static/js/1.1bdeb50c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "025e95c5386b70e84033b0f2543c2c1d",
+    "url": "/index.html"
+  }
+];
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 app.use(morgan('tiny'))
 app.use(bodyParser.json())
 app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
     {
